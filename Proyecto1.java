2522305d9d521065f02bac6fb253380bad2d118c
diegoadfp <157661254+diegoadfp@users.noreply.github.com>
2024-10-14 17:16:11 -0400

    Add files via upload

---
+++ Proyecto1.java
@@ -0,0 +1,17 @@
+
+package proyecto.pkg1;
+
+/**
+ *
+ * @author diego
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
